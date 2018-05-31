39b9e3ead8cf41d70f09f42cf2f33ab7df134f42
znlkws <ttxx.bnb1@gmail.com>
2018-05-31 15:49:55 +0800

    complete

--- main.js
+++ main.js
@@ -44,14 +44,23 @@ class Engine {
         }, 200);
     }
     gameStart() {
-        // 大盒子传到飞机中
+        // 大盒子属性传到飞机中
         plane.init(this.main);
         //飞机发射子弹
         setInterval(() => {
             plane.fire();
-        }, 500);
+        }, 300);
         //敌机诞生
-        new Enemy(this.main);
+        // new Enemy(this.main);
+        setInterval(() => {
+            if (Math.random() > 0.5) new Enemy(this.main, 1);
+        }, 1500);
+        setInterval(() => {
+            if (Math.random() > 0.7) new Enemy(this.main, 2);
+        }, 3000);
+        setInterval(() => {
+            if (Math.random() > 0.85) new Enemy(this.main, 3);
+        }, 6000);
     }
 }
 new Engine()
@@ -79,13 +88,14 @@ var plane = {
         }
     },
     fire: function () {
-        new Bullet();
-    }
+        // new Bullet();
+        this.aBullet.push(new Bullet());
+    },
+    aBullet: []
 }
-
+//子弹
 class Bullet {
     constructor() {
-
         this.init();
     }
     init() {
@@ -97,7 +107,7 @@ class Bullet {
     move() {
         this.ele.timer = setInterval(() => {
             this.ele.style.top = this.ele.offsetTop - 4 + 'px';
-            if (this.ele.offsetTop <= 100) {
+            if (this.ele.offsetTop <= 0) {
                 this.die();
             }
         }, 30);
@@ -117,21 +127,93 @@ class Bullet {
             }
             this.ele.style.backgroundImg = 'url(images/die' + i + '.png)'
         }, 50);
+        for (var j = 0; j < plane.aBullet.length; j++) {
+            if (plane.aBullet[j].ele == this.ele) {
+                plane.aBullet.splice(j, 1)
+                break;
+            }
+        }
+        // console.log(plane.aBullet);
     }
 }
-
+//敌机
 class Enemy {
-    constructor(main) {
+    constructor(main, value) {
         this.main = main;
+        this.value = value;
         this.init();
     }
     init() {
-        this.ele = createDiv('enemy-small');
+        switch (this.value) {
+            case 1:
+                this.ele = createDiv('enemy-small');
+                this.speed = 3;
+                this.img = 3;
+                this.num = 1;
+                this.hp = 2;
+                break;
+            case 2:
+                this.ele = createDiv('enemy-middle');
+                this.speed = 2;
+                this.img = 4;
+                this.num = 2;
+                this.hp = 5;
+                break;
+            case 3:
+                this.ele = createDiv('enemy-large');
+                this.speed = 1;
+                this.img = 6;
+                this.num = 3;
+                this.hp = 10;
+                break;
+        }
         this.ele.style.left = randomNum(this.main.offsetWidth - this.ele.offsetWidth, 0) + 'px';
         this.move()
     }
     move() {
+        this.ele.timer = setInterval(() => {
+            this.ele.style.top = this.ele.offsetTop + this.speed + 'px'
+            if (this.ele.offsetTop >= this.main.offsetHeight + this.ele.offsetHeight) {
+                this.die()
+            }
+            // 碰撞检测,根据敌机运动or子弹运动轨迹判断
+            // 遍历bullet判断位置
+            for (var i = 0; i < plane.aBullet.length; i++) {
+                //左
+                if (this.ele.offsetLeft - plane.aBullet[i].ele.offsetLeft < plane.aBullet[i].ele.offsetWidth) {
+                    // 右
+                    if (plane.aBullet[i].ele.offsetLeft < this.ele.offsetLeft + this.ele.offsetWidth) {
+                        console.log(1);
+                        // 中间下
+                        if (plane.aBullet[i].ele.offsetTop < this.ele.offsetTop + this.ele.offsetHeight) {
+                            //中间上
+                            if (plane.aBullet[i].ele.offsetTop > this.ele.offsetTop) {
+                                plane.aBullet[i].die();
+                                this.hp--;
+                                if (this.hp <= 0) {
+                                    this.die();
+                                }
+                            }
+                        }
+                    }
+                }
 
+            }
+        }, 30);
+    }
+    die() {
+        var i = 0;
+        setInterval(() => {
+            if (i == this.img) {
+                setTimeout(() => {
+                    this.ele.remove()
+                    clearInterval(this.ele.timer)
+                }, 80);
+            } else {
+                i++
+            }
+            this.ele.style.backgroundImage = 'url(images/plane' + this.num + '_die' + i + '.png)'
+        }, 50);
     }
 }
 
